88622721d6bd35d2f166e74fea12ccb1c3ce3580
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-08-05 11:23:43 +0100

    class variables

--- oop/main.py
+++ oop/main.py
@@ -1,17 +1,25 @@
 class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@company.com"
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return "{} {}".format(emp_1.first, emp_1.last)
 
+    def apply_raise(self):
+        self.pay = int(float(self.pay) * self.raise_amount)
+
+print(Employee.num_of_emps)
 
 emp_1 = Employee("Benjamin", "McGregor", "50000")
 emp_2 = Employee("Test", "Tester", "60000")
 
-ep_1.fullname()
-print(Employee.fullname(emp_1))
-# print(emp_2.fullname())
+print(Employee.num_of_emps)
