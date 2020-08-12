722c77baa08c704fc844343427ff6d51d8878c46
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-08-12 18:45:33 +0100

    special methods

--- oop/main.py
+++ oop/main.py
@@ -8,48 +8,34 @@ class Employee:
         self.pay = pay
         self.email = first + "." + last + "@company.com"
 
-
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(float(self.pay) * self.raise_amt)
 
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
-class Developer(Employee):
-    raise_amt = 1.10
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
 
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
+    def __add__(self, other):
+        return self.pay + other.pay
 
-
-class Manager(Employee):
-
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-
-    def add_employee(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-
-    def print_emps(self):
-        for emp in self.employees:
-            print("-->", emp.fullname())
+    def __len__(self):
+        return len(self.fullname())
 
 
-dev_1 = Developer("Benjamin", "McGregor", "50000", "Java")
-dev_2 = Developer("Test", "Tester", "60000", "Python")
+emp_1 = Employee("Benjamin", "McGregor", 50000)
+emp_2 = Employee("Test", "Tester", 60000)
 
-mgr_1 = Manager("Manny", "Manager", 90000, [dev_1])
+print(len(emp_1))
 
+# print(emp_1)
 
-print(issubclass(Manager, Developer))
+# print(repr(emp_1))
+# print(str(emp_1))
+
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
