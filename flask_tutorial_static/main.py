a7b1fe16da0fb864e49e19c3ce68922b4f00307d
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-08-26 13:03:49 +0100

    blueprints + multiple .py files

--- flask_tutorial_static/main.py
+++ flask_tutorial_static/main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, url_for
+from admin.second import second
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+app.register_blueprint(second, url_prefix="/admin")
 
 
-@app.route("/home")
 @app.route("/")
-def home():
-    return render_template("home.html")
+def test():
+    return "<h1>Test</h1?>"
 
 
 if __name__ == "__main__":
